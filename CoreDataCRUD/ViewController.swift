37e9135a5e644d77041f9bf06a549a7541e7feac
Filip <Filip>
2023-01-25 10:15:09 +0100

    Initial Commit

---
+++ CoreDataCRUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataCRUD
+//
+//  Created by Filip Rutkowski on 25/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
