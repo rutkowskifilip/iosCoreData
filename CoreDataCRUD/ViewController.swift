f5f19fb59ef0ee3c0c4d39bbeefe47b00ab81925
Filip <Filip>
2023-02-20 09:01:14 +0100

    adding user data check

--- CoreDataCRUD/ViewController.swift
+++ CoreDataCRUD/ViewController.swift
@@ -8,17 +8,21 @@ import CoreData
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-     
+    
     
     @IBOutlet weak var table: UITableView!
-    
+    var pesel:UITextField!
+    var name:UITextField!
+    var surname:UITextField!
+    var save:UIAlertAction!
+    var alert:UIAlertController!
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext   // właściwość
     var items:[Persons]?
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
-      
+        
         do{
             self.items = try self.context.fetch(Persons.fetchRequest())
             print("łotoott")
@@ -27,6 +31,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         // Do any additional setup after loading the view.
     }
+    
     var tab = ["a","b","c"];
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items?.count ?? 0;
@@ -44,10 +49,54 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-            let action = UIContextualAction(style: .destructive, title: "Delete") {(action, view, completionHandler) in
-                let toDelete = self.items![indexPath.row]
-                self.context.delete(toDelete)
+        let action = UIContextualAction(style: .destructive, title: "Delete") {(action, view, completionHandler) in
+            let toDelete = self.items![indexPath.row]
+            self.context.delete(toDelete)
+            
+            try! self.context.save()
+            do{
+                self.items = try self.context.fetch(Persons.fetchRequest())
+                print("łotoott")
+            }catch{
+                print("Error kurwa")
+            }
+            self.table.reloadData()
+            
+        }
+        let action2 = UIContextualAction(style: .normal, title: "Edit") {(action2, view, completionHandler) in
+            self.alert = UIAlertController(title: "Edytuj osobę:", message: "",                                      preferredStyle: .alert)
+            self.alert.addTextField(configurationHandler: { textField in
+                textField.placeholder = "PESEL"
+                textField.text = self.items?[indexPath.row].pesel ?? ""
+            })
+            
+            self.alert.addTextField(configurationHandler: { textField in
+                textField.placeholder = "Imię"
+                textField.text = self.items?[indexPath.row].name ?? ""
+            })
+            self.alert.addTextField(configurationHandler: { textField in
+                textField.placeholder = "Nazwisko"
+                textField.text = self.items?[indexPath.row].surname ?? ""
+            })
+
+            self.pesel = self.alert.textFields?[0]
+            self.name = self.alert.textFields?[1]
+            self.surname = self.alert.textFields?[2]
+            self.pesel.addTarget(self, action: #selector(self.checkData), for: UIControl.Event.editingChanged)
+            self.name.addTarget(self, action: #selector(self.checkData), for: UIControl.Event.editingChanged)
+            self.surname.addTarget(self, action: #selector(self.checkData), for: UIControl.Event.editingChanged)
+            
+            self.save = UIAlertAction(title: "Zapisz", style: .default, handler: { [weak alert = self.alert] (_) in
+                var pesel = self.alert!.textFields![0].text
+                let peselLenght = pesel?.count == 11
                 
+                
+                
+//                let newPerson = Persons(context: self.context)
+//                newPerson.pesel = self.alert!.textFields![0].text
+//                newPerson.name = self.alert!.textFields![1].text
+//                newPerson.surname = self.alert!.textFields![2].text
+               
                 try! self.context.save()
                 do{
                     self.items = try self.context.fetch(Persons.fetchRequest())
@@ -56,45 +105,51 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     print("Error kurwa")
                 }
                 self.table.reloadData()
-               
-            }
-        let action2 = UIContextualAction(style: .normal, title: "Edit") {(action2, view, completionHandler) in
-            print("edit")
-
-        }
-            return UISwipeActionsConfiguration(actions: [action, action2])
-        }
-
-    @IBAction func addPersonButton(_ sender: Any) {
-       
-    
-           
-           // Create and configure the alert controller.
-        let alert = UIAlertController(title: "Dodaj osobę:", message: "",
-               
-                 preferredStyle: .alert)
-            alert.addTextField(configurationHandler: { textField in
-                textField.placeholder = "PESEL"
-                })
-            alert.addTextField(configurationHandler: { textField in
-                textField.placeholder = "Imie"
-                })
-            alert.addTextField(configurationHandler: { textField in
-                textField.placeholder = "Nazwisko"
-                })
-        let defaultAction = UIAlertAction(title: "Zapisz",
-                                          style: .default) { (action) in
+                
+            })
+            self.save.isEnabled = false
+            self.alert.addAction(self.save)
             
-           
-            // Respond to user selection of the action.
-            try! self.context.save()
-           }
-           let cancelAction = UIAlertAction(title: "Anuluj",
-                                style: .cancel) { (action) in
-            // Respond to user selection of the action.
-           }
+            self.alert.addAction(UIAlertAction(title: "Anuluj", style: .default, handler: { [weak alert = self.alert] (_) in
+                
+                
+            }))
+                                         
+            self.present(self.alert, animated: true) {
+                // The alert was presented
+            }
+            
+        }
+        return UISwipeActionsConfiguration(actions: [action, action2])
+    }
+    
+    @IBAction func addPersonButton(_ sender: Any) {
         
-        alert.addAction(UIAlertAction(title: "Zapisz", style: .default, handler: { [weak alert] (_) in
+        
+        
+        
+        // Create and configure the alert controller.
+        alert = UIAlertController(title: "Dodaj osobę:", message: "",
+                                      
+                                      preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "PESEL"
+        })
+        
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Imie"
+        })
+        alert.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Nazwisko"
+        })
+        self.pesel = alert.textFields?[0]
+        self.name = alert.textFields?[1]
+        self.surname = alert.textFields?[2]
+        pesel.addTarget(self, action: #selector(checkData), for: UIControl.Event.editingChanged)
+        name.addTarget(self, action: #selector(checkData), for: UIControl.Event.editingChanged)
+        surname.addTarget(self, action: #selector(checkData), for: UIControl.Event.editingChanged)
+        
+        self.save = UIAlertAction(title: "Zapisz", style: .default, handler: { [weak alert] (_) in
             var pesel = alert!.textFields![0].text
             let peselLenght = pesel?.count == 11
             
@@ -114,32 +169,53 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
             self.table.reloadData()
             
-        }))
+        })
+        self.save.isEnabled = false
+        alert.addAction(self.save)
         
         alert.addAction(UIAlertAction(title: "Anuluj", style: .default, handler: { [weak alert] (_) in
-   
-       
+            
             
         }))
-                
-           self.present(alert, animated: true) {
-              // The alert was presented
-           }
+        
+        self.present(alert, animated: true) {
+            // The alert was presented
+        }
+        
     }
-    // właściwość
-   
-    // np. dodanie danych
- 
-
-
-    /* //usuwanie
-        let toDelete = self.items![0]
-        self.context.delete(toDelete)
-    */
-
-// a mozna w pełen blok try catch jak ponizej
-
-    // fetch danych
-    
+    @objc func checkData(){
+        var peselStr:String = pesel.text ?? ""
+        var peselArr:[String.SubSequence] = peselStr.split(separator: "")
+        if(peselStr.count != 11 || name.text?.count ?? 0 < 1 || surname.text?.count ?? 0 < 1){
+            save.isEnabled = false
+        }else{
+            var sum:Int = 0
+            var arr :[Int] = [1,3,7,9,1,3,7,9,1,3]
+            for i in 0...9{
+                sum += (Int(peselArr[i]) ?? 0)*arr[i]%10
+            }
+            sum = sum % 10
+            if(10 - sum == Int(peselArr[10])){
+                save.isEnabled = true
+            }
+            
+        }
+        
+        
+        // właściwość0
+        // np. dodanie danych
+        
+        
+        
+        /* //usuwanie
+         let toDelete = self.items![0]
+         self.context.delete(toDelete)
+         */
+        
+        // a mozna w pełen blok try catch jak ponizej
+        
+        // fetch danych
+        
+    }
 }
 
